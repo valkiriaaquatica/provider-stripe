d4f408041cdffb8807afc1f461e41db826bb141f
valkiriaaquatica <fernandomendietaovejero@gmail.com>
2025-12-17 02:18:18 +0100

    feat: add ci

--- config/cluster/stripecustomer/config.go
+++ config/cluster/stripecustomer/config.go
@@ -2,7 +2,6 @@ package stripecard
 
 import (
 	ujconfig "github.com/crossplane/upjet/v2/pkg/config"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 // Configure configures resources for the virtual environment group
@@ -13,8 +12,8 @@ func Configure(p *ujconfig.Provider) {
 		// The provider marks CVC as sensitive but defines it as a number,
 		// which upjet cannot map to a Secret. Treat it as a string to
 		// generate the CRD successfully.
-		if cvc := ujconfig.GetSchema(r.TerraformResource, "cvc"); cvc != nil {
-			cvc.Type = schema.TypeString
-		}
+		// if cvc := ujconfig.GetSchema(r.TerraformResource, "cvc"); cvc != nil {
+		// 	cvc.Type = schema.TypeString
+		// }
 	})
 }
